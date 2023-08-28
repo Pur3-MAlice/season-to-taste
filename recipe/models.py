16a574c0f4c95504076d0a68e5a7a883def359a1
Pur3-MAlice <aliceford98@live.co.uk>
2023-08-28 21:58:01 +0000

    adds models.py base for the views to work off of

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    season_tag = models.CharField(max_length=15)
+    diet_tag = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.season_tag}, {self.diet_tag}"
+
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    ingredients = models.TextField()
+    calories = models.IntegerField(default=0)
+    fat = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0)
+    carbs = models.IntegerField(default=0)
+    servings = models.IntegerField(default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipes"
+    )
+    categories = models.ManyToManyField(
+        Category, related_name="recipes"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    rate = models.ManyToManyField(User, related_name='rate')
+    saved = models.ForeignKey(
+        Saved, on_delete=models.CASCADE, related_name="saved"
+        )
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class SavedRecipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+class RecipeRating(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    # rating scale 0 to 5 figure out later?
