25a49c0976007c3a288b499f236729d9a68a9512
Pur3-MAlice <aliceford98@live.co.uk>
2023-08-28 20:32:00 +0000

    adds folders for future media and Procfile for deployment

--- seasontotaste/settings.py
+++ seasontotaste/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-pur3malice-seasontotast-2uqbhn73kny.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['season-to-taste.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'seasontotaste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
